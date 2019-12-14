b5349fb4dd36add5117ee2490164af641f94d072
Storm atronach Robert frey <srobert-@su-b2.21-school.ru>
2019-12-14 18:23:06 +0300

    merge

--- srcs/analyze.c
+++ srcs/analyze.c
@@ -6,24 +6,24 @@
 /*   By: srobert- <srobert-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/11 22:34:33 by david             #+#    #+#             */
-/*   Updated: 2019/12/14 18:20:46 by srobert-         ###   ########.fr       */
+/*   Updated: 2019/12/14 18:22:38 by srobert-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/head.h"
 
-void ignore_flags(t_flags *flags, int len)
-{
-	int tmp_width;
-	int tmp_f_num;
+// void ignore_flags(t_flags *flags, int len)
+// {
+// 	int tmp_width;
+// 	int tmp_precision;
 
-	if (flags->minus)
-		flags->zero = 0;
-	tmp_width = (flags->width > flags->f_num) ? flags->width - flags->f_num : 0;
-	tmp_f_num = (flags->f_num > len) ? flags->f_num - len : 0;
-	// if
+// 	if (flags->minus)
+// 		flags->zero = 0;
+// 	tmp_width = (flags->width > flags->precision) ? flags->width - flags->precision : 0;
+// 	tmp_precision = (flags->precision > len) ? flags->precision - len : 0;
+// 	// if
 
-}
+// }
 
 void	analyze_digit(t_flags flags, int arg)
 {
@@ -33,7 +33,7 @@ void	analyze_digit(t_flags flags, int arg)
 
 	len = num_len(arg);
 	num = ft_itoa(arg);
-	ignore_flags(&flags, len);
+	// ignore_flags(&flags, len);
 	ft_putnbr(arg);
 	debug_flags(flags);
 }
