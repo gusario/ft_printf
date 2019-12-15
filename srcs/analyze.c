b0acc7d04e27381df56e3bc8ec4cfdb812f492f1
Storm atronach Robert frey <srobert-@su-b4.21-school.ru>
2019-12-15 20:34:18 +0300

    meh

--- srcs/analyze.c
+++ srcs/analyze.c
@@ -6,34 +6,32 @@
 /*   By: srobert- <srobert-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/11 22:34:33 by david             #+#    #+#             */
-/*   Updated: 2019/12/14 18:22:38 by srobert-         ###   ########.fr       */
+/*   Updated: 2019/12/15 20:33:26 by srobert-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/head.h"
 
-// void ignore_flags(t_flags *flags, int len)
-// {
-// 	int tmp_width;
-// 	int tmp_precision;
-
-// 	if (flags->minus)
-// 		flags->zero = 0;
-// 	tmp_width = (flags->width > flags->precision) ? flags->width - flags->precision : 0;
-// 	tmp_precision = (flags->precision > len) ? flags->precision - len : 0;
-// 	// if
-
-// }
+void	recalc_flags(t_flags *flags, int len)
+{
+	if (flags->minus)
+		flags->zero = 0;
+	flags->precision = (flags->precision > len) ? flags->precision - len : 0;
+	flags->width = (flags->width > len + flags->precision) ? flags->width - \
+	(len + flags->precision) : 0;
+	if (flags->plus)
+		flags->width = flags->width > 0 ? flags->width-- : 0;
+}
 
 void	analyze_digit(t_flags flags, int arg)
 {
-	char	*buff;
-	char	*num;
-	int		len;
+	int	len;
+	int	i;
 
+	i = 0;
 	len = num_len(arg);
-	num = ft_itoa(arg);
-	// ignore_flags(&flags, len);
+	recalc_flags(&flags, len);
+	
 	ft_putnbr(arg);
 	debug_flags(flags);
 }
